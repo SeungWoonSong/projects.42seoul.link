1d8768111b7e7ec59c5f07a6853fcf2f3d5afc71
SeungWoonSong <susong@student.42seoul.kr>
2023-03-21 22:20:48 +0900

    feat : Re render every 30seconds

--- front/pages/index.js
+++ front/pages/index.js
@@ -1,15 +1,38 @@
 import { getRows } from '../lib/db';
+import { useEffect } from 'react';
 
 export async function getStaticProps() {
-  const rows = await getRows();
+  let rows = null;
+  try {
+    const result = await getRows(); // 'getRows' 함수를 사용하도록 변경
+    rows = result.map(row => ({
+      ...row,
+      start_time: row.start_time.toISOString(), // JSON 직렬화 가능한 형식으로 변경
+      error_time: row.error_time,
+      working_time: row.working_time,
+      last_checked: row.last_checked.toISOString(),
+    }));
+  } catch (error) {
+    console.error("Error fetching data:", error);
+  }
+
   return {
     props: {
-      rows,
+      rows: rows || null,
     },
+    revalidate: 60 * 1,
   };
 }
 
+
+
 export default function Home({ rows }) {
+  useEffect(() => {
+    const intervalId = setInterval(() => {
+      window.location.reload();
+    }, 30000); // 1분마다 새로고침
+    return () => clearInterval(intervalId);
+  }, []);
   return (
     <div>
       <h1>데이터베이스 행 정보</h1>
@@ -27,7 +50,7 @@ export default function Home({ rows }) {
           </tr>
         </thead>
         <tbody>
-          {rows.map((row) => (
+          {rows && rows.map((row) => ( // rows가 존재하는지 확인
             <tr key={row.id}>
               <td>{row.id}</td>
               <td>{row.site_name}</td>
